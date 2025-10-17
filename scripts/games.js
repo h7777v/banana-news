6d6902af33c066075b9065686fe77916a4143feb
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-10-17 20:28:44 +1100

    Games.js update

--- scripts/games.js
+++ scripts/games.js
@@ -32,7 +32,9 @@ https://banana-news.github.io/banana/share_this_page
 var jsonFile
 var layout
 
-(async()=>{await fetch('../data/games.json').then(response=>{if(!response.ok){throw new Error(`HTTP error retrieving games.json!Status: ${response.status}`);}return response.json();}).then(data=>jsonFile=data;for(i in jsonFile){layout+=`<div class="options-div" title="${jsonFile[i].title}" style="background-image:url('${jsonFile[i].img}')"><h2>${jsonFile[i].header}</h2><p>${jsonFile[i].text}</p></div>`}).catch(error => console.error('Unable to fetch data (games.json):', error));})();
+const loadLayout=(json)=>{jsonFile=json;for(i in jsonFile){layout+=`<div class="options-div" title="${jsonFile[i].title}" style="background-image:url('${jsonFile[i].img}')"><h2>${jsonFile[i].header}</h2><p>${jsonFile[i].text}</p></div>`}
+
+(async()=>{await fetch('../data/games.json').then(response=>{if(!response.ok){throw new Error(`HTTP error retrieving games.json!Status: ${response.status}`);}return response.json();}).then(data=>loadLayout(data)}).catch(error => console.error('Unable to fetch data (games.json):', error));})();
 
 console.log(jsonFile);
 
