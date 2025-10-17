5e022b20ebce15fd6e0bdd40bc6e6db4216a774d
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-10-17 19:47:13 +1100

    Games.js update

--- scripts/games.js
+++ scripts/games.js
@@ -34,9 +34,11 @@ var layout
 
 (async()=>{await fetch('../data/games.json').then(response=>{if(!response.ok){throw new Error(`HTTP error retrieving games.json!Status: ${response.status}`);}return response.json();}).then(data=>jsonFile=data).catch(error => console.error('Unable to fetch data (games.json):', error));})();
 
+console.log(jsonFile);
+
 layout=""
 
-for(i of jsonFile){
+for(i in jsonFile){
   layout+=`<div class="options-div" title="${jsonFile[i].title}" style="background-image:url('${jsonFile[i].img}')"><h2>${jsonFile[i].header}</h2><p>${jsonFile[i].text}</p></div>`
 }
 
