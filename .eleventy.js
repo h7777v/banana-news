3f048cc3cf5f015bd8ccef1c43330cd156c169fb
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-10-19 15:24:37 +1100

    Server config added.

--- .eleventy.js
+++ .eleventy.js
@@ -4,4 +4,5 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("shareimages");
   eleventyConfig.addPassthroughCopy("data");
+  eleventyConfig.addPassthroughCopy("server");
 };
