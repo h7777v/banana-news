3eafaefefaf2a0e0f6fda446cbfd33c930df3c78
Albert Kemp <albertkemp11@gmail.com>
2025-10-21 17:23:38 +1100

    s

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,13 +1,41 @@
-var stuff = document.getElementById('codeInput').value;
-var output = document.getElementById('output');
-var lines = stuff.split("\n");
-var html = "";
-for (i in lines) {
-    if (lines[i].startsWith("# ")) {
-        html+=`<h1>${lines[i].slice(2)}</h1>`;
-    } else if (lines[i].startsWith("## ")) {
-        html+=`<h2>${lines[i].slice(3)}</h2>`;
-    } else {
-        html+=`<p>${lines[i]}</p>`;
+function convert() {
+    var stuff = document.getElementById('codeInput').value;
+    var lines = stuff.split("\n").replace(/</gi, "&lt;").replace(/>/gi, "&gt;")
+    var html = ""
+    var outputDiv = document.getElementById('output');
+
+    for (i in lines) {
+        if (lines[i].startsWith("# ")) {
+            lines[i] = `<h1>${lines[i].slice(2)}</h1>`;
+            html+=lines[i]
+        } else if (lines[i].startsWith("## ")) {
+            lines[i] = `<h2>${lines[i].slice(3)}</h2>`;
+            html+=lines[i]
+        } else {
+            if (lines[i].includes("**")) {
+                while (lines[i].includes("**")) {
+                    lines[i] = lines[i].split("**", 1).join("<b>");
+                    lines[i] = lines[i].split("**", 1).join("</b>");
+                }
+                lines[i] = `<p>${lines[i]}</p>`;
+                html+=lines[i];
+            } else if (lines[i].includes("_")) {
+                while (lines[i].includes("_")) {
+                    lines[i] = lines[i].split("_", 1).join("<i>");
+                    lines[i] = lines[i].split("_", 1).join("</i>");
+                }
+                lines[i] = `<p>${lines[i]}</p>`;
+                html+=lines[i];
+            } else if (lines[i].includes("`")) {
+                while (lines[i].includes("`")) {
+                    lines[i] = lines[i].split("`", 1).join("<code>");
+                    lines[i] = lines[i].split("`", 1).join("</code>");
+                }
+                lines[i] = `<p>${lines[i]}</p>`;
+                html+=lines[i];
+            }
+
+        }
     }
+    outputDiv.innerHTML = `<p>${html}</p>`;
 }
