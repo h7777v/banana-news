b631fdba0b196ebb0174959d7e367a120a6ddeb6
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-10-26 19:59:03 +1100

    no more handlebars

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,4 @@
-const handlebarsPlugin = require("@11ty/eleventy-plugin-handlebars");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(handlebarsPlugin);
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("scripts");
   eleventyConfig.addPassthroughCopy("images");
