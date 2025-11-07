672f6643bdf0ef3a4dfd17714841c646cd6f4fe2
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-11-07 21:38:12 +1100

    3 way toggle.

--- scripts/settings.js
+++
@@ -1,8 +0,0 @@
-const tri = document.getElementById("button-3way");
-const range = document.getElementById("triRange");
-
-function updateState() {
-  tri.dataset.state = range.value;
-}
-range.addEventListener("input", updateState);
-updateState();
