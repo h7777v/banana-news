216d4700d76627614af6f2f135fa2d75ea99580a
Haoyang Zhou <132132792+h7777v@users.noreply.github.com>
2025-11-16 16:59:36 +1100

    Flat minecraft! test.

--- local_frame/flat_minecraft/script.js
+++ local_frame/flat_minecraft/script.js
@@ -15,3 +15,9 @@ function updatePage(setState){
     };
     pages[state].style.display = 'block';
 }
+
+const init = () => {
+    updatePage(0);
+}
+
+document.addEventListener("DOMContentLoaded", init);
