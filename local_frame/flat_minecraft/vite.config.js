699aadd7d4d0787af6d72195629df08c715e2c2e
h7777v <132132792+h7777v@users.noreply.github.com>
2025-11-18 16:56:35 +1100

    FMC!!!!
    dgoisehrgkjxbkjfvkxjvbfj

--- local_frame/flat_minecraft/vite.config.js
+++ local_frame/flat_minecraft/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/local_frame/flat_minecraft', 
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        game: resolve(__dirname, 'game.html')
+      }
+    },
+    outDir: 'dist'
+  }
 })
