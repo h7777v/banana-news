ef356f82b2d261f189e2e5902e69104d49b07910
h7777v <132132792+h7777v@users.noreply.github.com>
2025-11-18 16:18:47 +1100

    srgdfgdsrg
    dgfdgsdrgsdrgfvstrhggrfhsrdtbgftrgfbvcgfbvfdxcrfg

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        myGame: path.resolve(__dirname, 'local_frame/flat_minecraft/game.html')
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
 });
